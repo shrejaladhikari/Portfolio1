aa07a76b8e11c3ecc11f75ddf1a587422fead33d
Shrejal Adhikari <s.adhikari.598@westcliff.edu>
2024-12-16 13:15:41 +0545

    Initial commit: Flask Portfolio Project

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
+
+@app.route('/projects')
+def projects():
+    project_data = [
+        {
+            "title": "Song Recommendation System",
+            "description": "A Machine Learning-powered system that recommends songs based on user input. The backend is built with Flask and integrated with Spotify's API for album covers, while the React.js frontend displays personalized recommendations interactively.",
+            "links": [
+                {"name": "Backend", "url": "https://songrecommendation-final.onrender.com"},
+                {"name": "Frontend", "url": "https://songrecommendation-final-1.onrender.com"}
+            ]
+        },
+        {
+            "title": "Movie Recommendation System",
+            "description": "A Machine Learning-based system to suggest movies based on user preferences, enhancing personalized viewing experiences.",
+            "links": [
+                {"name": "View Project", "url": "https://movierecommendationsystem-xfk7.onrender.com"}
+            ]
+        },
+        {
+            "title": "Query Relevance and Similarity",
+            "description": "An NLP-based tool to analyze query relevance and measure document similarity for efficient search and retrieval.",
+            "links": [
+                {"name": "View Project", "url": "https://similarityreport-queryrelevance.onrender.com"}
+            ]
+        }
+    ]
+    return render_template('projects.html', projects=project_data)
+
+
+# Contact Page
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var typed = new Typed("#typed-output", {
+    strings: ["Hi There, I'm Shrejal Adhikari."],
+    typeSpeed: 50, // Typing speed
+    backSpeed: 30, // Backspacing speed
+    loop: true, // Do not loop
+    showCursor: true, // Show blinking cursor
+    cursorChar: "|", // Cursor character
+  });
+});
